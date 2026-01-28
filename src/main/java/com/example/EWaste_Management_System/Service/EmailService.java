1a7ad30a25293060c425c7ba6a02375f91d8e1d6
tchandrashekar <tanneeruchandrashekar007@gmail.com>
2026-01-28 17:16:08 +0530

    updated email configuration

--- src/main/java/com/example/EWaste_Management_System/Service/EmailService.java
+++ src/main/java/com/example/EWaste_Management_System/Service/EmailService.java
@@ -15,10 +15,24 @@ public class EmailService {
     private final JavaMailSender mailSender;
 
     public void sendEmail(String to, String subject, String body) {
+        /*
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(to);
         msg.setSubject(subject);
         msg.setText(body);
         mailSender.send(msg);
+*/
+                try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            message.setFrom("yourgmail@gmail.com");
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            System.out.println("Email failed: " + e.getMessage());
+        }
+
     }
 }
